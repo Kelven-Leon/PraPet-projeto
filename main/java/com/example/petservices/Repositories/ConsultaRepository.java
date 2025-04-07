4591d9fb570160da0d9593740642bc54e1427969
Kelven-Leon <kelven_tjs_@hotmail.com>
2025-04-07 20:14:04 -0300

    Update ConsultaRepository.java
    Commit na branch principal.

--- main/java/com/example/petservices/Repositories/ConsultaRepository.java
+++ main/java/com/example/petservices/Repositories/ConsultaRepository.java
@@ -1,6 +1,6 @@
-package com.example.petservices.Repositories;
+package br.com.PojetoPraPets.Repositories;
 
-import com.example.petservices.Models.Consulta;
+import br.com.PojetoPraPets.Models.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +8,3 @@ import org.springframework.stereotype.Repository;
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
     // Métodos customizados para Consulta
 }
-
